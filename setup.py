f5d8d8ce48b833edaccbb2f61ce00de2905af211
pedroten <pedro.ten@stoneworksolutions.net>
2015-12-22 13:51:30 +0100

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='stoneredis',
-    version='0.2.0',
+    version='0.2.1',
     description="Redis client based on redis-py client with some added features",
     long_description=readme + '\n\n' + history,
     author="Pedro Ten",
--- stoneredis/__init__.py
+++ stoneredis/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Pedro Ten'
 __email__ = 'pedroten@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
