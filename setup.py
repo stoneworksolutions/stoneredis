7cc0711ec1cb34f82184c1969bf0a6de8e34ef54
deroky <mariocoder@gmail.com>
2016-01-19 11:39:50 +0100

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='stoneredis',
-    version='0.4.0',
+    version='0.5.0',
     description="Redis client based on redis-py client with some added features",
     long_description=readme + '\n\n' + history,
     author="Pedro Ten",
