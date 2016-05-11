ba848449b967d7dbc0e24e25fe858db33a43f994
jfarranz <jessica.farranz@gmail.com>
2016-05-11 11:53:35 +0200

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='stoneredis',
-    version='0.5.2',
+    version='0.6.0',
     description="Redis client based on redis-py client with some added features",
     long_description=readme + '\n\n' + history,
     author="Pedro Ten",
--- stoneredis/__init__.py
+++ stoneredis/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Pedro Ten'
 __email__ = 'pedroten@gmail.com'
-__version__ = '0.5.2'
+__version__ = '0.6.0'
 
 from client import StoneRedis
